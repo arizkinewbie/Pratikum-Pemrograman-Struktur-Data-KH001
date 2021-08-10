c89335f558ae6ab1c2619802d1e7074c9eb6bfe3
Arizki Putra Rahman <arizkijitsu@gmail.com>
2021-08-10 14:44:15 +0000

    Add file main.c

--- main.c
+++ main.c
@@ -4,17 +4,6 @@ char huruf[30], reversed[30];
 int i, nHuruf;
 
 int main(void) {
-  printf("Masukkan nama (string):\n");
-fgets(huruf, sizeof(huruf), stdin);
-long int length = strlen(huruf);
-
-//Reverse string
-for(i=0; i<length; ++i) {
-  reversed[length-1-i] = huruf[i];
-}
-
-printf("Reversed string : %s\n",reversed);
-printf("Jumlah : %d\n",i);
-
+  printf("Hello World!");
 return 0;
 }

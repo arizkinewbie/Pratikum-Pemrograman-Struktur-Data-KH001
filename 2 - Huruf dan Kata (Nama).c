a73357e9ef31163898c10bfde96bcca6c5da8afd
Arizki Putra Rahman <arizkijitsu@gmail.com>
2021-08-14 08:17:20 +0000

    Add boolean and else

--- 2 - Huruf dan Kata (Nama).c
+++ 2 - Huruf dan Kata (Nama).c
@@ -1,24 +1,17 @@
-/*
-	Name: Program Reverse Kata dan Huruf (Nama)
-	Copyright: Sabeb (ArizkiNewbie)
-	Author: Arizki Putra Rahman
-	Date: 10/08/21 23:01
-	Description: Mulai Aja DUlu
-*/
-
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 char huruf[50], kata[50];
-int i, j, nInput, nHuruf, nKata;
+int i, j, k, nInput, nHuruf, nKata;
 
 void Huruf() {
 	//hitung nHuruf
 	while (huruf[i] != '\0') {
 		if(huruf[i] != ' ') {
 			nHuruf = nHuruf + 1;
-		};
+		}
 		i++;
-	};
+	}
 	//output nama terbaik dan jumlah huruf
     printf("\nNama terbalik (Reversed) \t: %s\n", strrev(huruf));
     printf("Jumlah Huruf \t\t\t: %d\n", nHuruf);
@@ -26,21 +19,33 @@ void Huruf() {
 
 void Kata(){
 	//cetak kata dari belakang dan hitung nKata
+	bool cek = 1;
 	printf("\nUrutan nama dari kata belakang  : ");
+
 	for(i=nInput-1; i>=-1; i--){
         if(kata[i] == ' ' || i<0){
             j = i;
-            while(j+1 <= nInput){ 
-            	printf("%c", kata[j+1]);
-                j++;
-        	};
+            if (cek) {
+            	while(j+1 <= nInput){ 
+            		printf("%c", kata[j+1]);
+              	  j++;
+        		}
+        		cek = 0;
+        	}
+        	else {
+        		while(k <= nInput) {
+        			printf("%c", kata[k]);
+              	  k++;
+				}
+			}
         nKata++;
         nInput = nInput-(nInput-i);
-        };
-	};
+        }
+	}
+    
 	//cetak jumlah kata
 	printf("\nJumlah Kata \t\t\t: %d", nKata);
-};
+}
 
 int main() {
     printf("Masukkan nama (string) \t\t: ");
@@ -50,4 +55,4 @@ int main() {
     // panggil fungsi
 	Huruf(); Kata();
     return 0;
-};
+}
